14538598fee2a4646700119028f05e4b2e2ed731
lllIllIl <119816195+lllIllIl@users.noreply.github.com>
2022-12-13 23:28:24 -0700

    commitial init
    ok

---
+++ scripts/rarity.R
@@ -0,0 +1,19 @@
+rarity <- function(x){
+  #this function takes as "x" a table of presence/absence data where rows are
+  #sites and columns are artifact types
+  degun <- as.data.frame(matrix(NA, 2, ncol(x)))
+  colnames(degun) <- colnames(x)
+  for (i in seq_len(ncol(x))){
+    degun[1,i] <- sum(x[,i])
+  }
+  degnorm <- as.data.frame(degun)
+  for (i in seq_len(ncol(x))){
+    degnorm[1,i] <- (degun[1,i]/nrow(x))
+  }
+  for (i in seq_len(ncol(x))){
+    degnorm[2,i] <- abs(degnorm[1,i]-1)
+  }
+  degout <- as.data.frame(t(degnorm))
+  colnames(degout) <- c("Spread","Inverse")
+  return(degout)
+}
